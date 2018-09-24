f18283aa1893296480f95d62f49975e1b961cf4b
joanlamrack <joanlamrack@gmail.com>
2018-09-24 16:39:52 +0700

    Add Edit Profile

--- src/containers/EditProfile.js
+++ src/containers/EditProfile.js
@@ -1,37 +1,187 @@
-import React, { Component } from 'react'
-import { 
-  View, 
-  StyleSheet,
-} from 'react-native'
+import React, { Component } from "react";
+import { View, StyleSheet, Image, Alert } from "react-native";
 import {
-  Text, 
-  Button 
-} from 'native-base'
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Text,
+  Spinner
+} from "native-base";
 
-export default class EditProfile extends Component {
-  static navigationOptions = {
-    title: 'EditProfile',
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button rounded info onPress={() => {
-          this.props.navigation.navigate('Profile')
-          }}
-        >
-          <Text> BACK </Text>
-        </Button>
-      </View>
-    );
-  }
-
-}
+import Icon from "react-native-vector-icons/Ionicons";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#bfe4ff"
   },
+  editTitle: {
+    justifyContent: "center",
+    flex: 1
+  },
+  form: {
+    flex: 2,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10
+  },
+  confirmButton: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  button: {
+    width: "90%",
+    flex: 1,
+    alignSelf: "center"
+  }
 });
+
+export default class FloatingLabelExample extends Component {
+  static navigationOptions = {
+    title: "Edit Profile"
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      name: "Suuusi",
+      email: "sushi@parcelpintar.com",
+      phone: "+62853119062",
+      loading: false,
+      success: null
+    };
+  }
+
+  render() {
+    let button = {};
+
+    let successButton = (
+      <Button
+        rounded
+        success
+        style={styles.button}
+        onPress={() => {
+          this.setState({
+            loading: false,
+            success: null
+          });
+        }}
+      >
+        <Icon name="ios-checkmark-circle-outline" color="white" size={30} />
+        <Text>Saved</Text>
+      </Button>
+    );
+
+    let failedButton = (
+      <Button
+        rounded
+        danger
+        style={styles.button}
+        onPress={() => {
+          this.setState({
+            loading: false,
+            success: null
+          });
+        }}
+      >
+        <Icon name="ios-alert" color="white" size={30} />
+        <Text>Failed</Text>
+      </Button>
+    );
+
+    let submitButton = (
+      <Button
+        rounded
+        info
+        style={styles.button}
+        onPress={() => {
+          this.setState({
+            loading: true
+          });
+        }}
+      >
+        <Icon name="ios-person" color="white" size={30} />
+        <Text>Submit</Text>
+      </Button>
+    );
+
+    let loadingButton = (
+      <Button
+        rounded
+        info
+        style={styles.button}
+        onPress={() => {
+          let choose = Math.floor(Math.random() * Math.floor(2));
+          if (choose) {
+            this.setState({
+              loading: false,
+              success: true
+            });
+          } else {
+            this.setState({
+              loading: false,
+              success: false,
+            });
+          }
+        }}
+      >
+        <Spinner color="white" />
+        <Text>Loading...</Text>
+      </Button>
+    );
+
+    if (!this.state.loading && this.state.success===null) {
+      button = submitButton;
+    } else if (this.state.success && !this.state.loading) {
+      button = successButton;
+    } else if (this.state.success===false && !this.state.loading) {
+      button = failedButton;
+    } else if (this.state.loading) {
+      button = loadingButton;
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.editTitle}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{
+              uri:
+                "http://www.myiconfinder.com/uploads/iconsets/ac7256a56da1fa7c09a699ddec407e7e-human.png"
+            }}
+          />
+        </View>
+        <View style={styles.form}>
+          <Form>
+            <Item stackedLabel>
+              <Label>Name</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label>Email</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label>Phone</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </View>
+        <View style={styles.confirmButton}>{button}</View>
+      </View>
+    );
+  }
+}
