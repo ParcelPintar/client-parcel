4205bfe414f0f2b6f62ed50c0e4f26672d3f4743
kneric <brian.pradipta.a@gmail.com>
2018-09-25 13:09:56 +0700

    working on order

--- src/containers/ConfirmOrder.js
+++ src/containers/ConfirmOrder.js
@@ -2,23 +2,40 @@ import React, { Component } from 'react'
 import { 
   View, 
   StyleSheet,
+  Text
 } from 'react-native'
 import {
-  Text, 
+  // Text, 
   Button 
 } from 'native-base'
 
 export default class ConfirmOrder extends Component {
+  constructor(){
+    super()
+    this.state = {
+      distance: null,
+      ETA: null,
+      item: null
+    }
+  }
+
   static navigationOptions = {
     title: 'Order Detail',
   };
 
+  componentDidMount = () => {
+    this.setState({
+      distance: this.props.navigation.getParam('distance'),
+      ETA: this.props.navigation.getParam('ETA'),
+      item: this.props.navigation.getParam('item')
+    })
+  }
+  
   render() {
+    console.log(this.state);
     return (
       <View style={styles.container}>
-        <Text>{JSON.stringify(this.props.navigation.getParam('distance'))}</Text>
-        <Text>{JSON.stringify(this.props.navigation.getParam('ETA'))}</Text>
-        <Text>{JSON.stringify(this.props.navigation.getParam('item'))}</Text>
+        <Text>AA</Text>
       </View>
     );
   }
